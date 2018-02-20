ed9602f6523412c4b246d561401ce2913e72287c
supanee <supanee.co59@snru.ac.th>
2018-02-20 11:32:15 +0700

    Create Fragment Success

---
+++ app/src/main/java/supanee/snru/ac/th/emercall/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package supanee.snru.ac.th.emercall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import supanee.snru.ac.th.emercall.R;
+
+/**
+ * Created by Admin on 20/2/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_poilce, container, false);
+        return view;
+    }
+}//Main class
